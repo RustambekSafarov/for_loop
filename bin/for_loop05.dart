9a5fd56f121bd3a5cec11f68225255cbe4ef2e10
Rustambek <rustambeksafarovv@gmail.com>
2022-07-19 18:17:26 +0500

    Redone

--- bin/for_loop05.dart
+++ bin/for_loop05.dart
@@ -1,4 +1,12 @@
 int func(List<int> numbers) {
+  int x = numbers.length;
+  int y = 0;
+  for(int i = 0; x > i; i += 1){
+    if(numbers[i].isOdd){
+      y += numbers[i];
+    }
+  }
+
 /*
    Given the list of numbers, return sum the odd numbers in the list
 Args:
