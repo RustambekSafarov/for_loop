9192784f55e30f20da038765904d16876dac450f
Rustambek <rustambeksafarovv@gmail.com>
2022-07-19 18:14:52 +0500

    Redone

--- bin/for_loop04.dart
+++ bin/for_loop04.dart
@@ -1,4 +1,11 @@
 int func(List<int> numbers) {
+  int x = numbers.length;
+  int y = 0;
+  for(int i = 0; x > i; i += 1){
+    if(numbers[i].isEven){
+      y += numbers[i];
+    }
+  }
 /*
    Given the list of numbers, return the sum the even numbers in the list
 Args:
@@ -6,7 +13,7 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+  return y;
 }
 
 void main() {}
